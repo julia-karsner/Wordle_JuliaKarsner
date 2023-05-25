b1364b78382afcdfb2f51267d1089e779320cc28
julia-karsner <25jkarsner@castilleja.org>
2023-05-24 19:47:11 -0700

    I commented out all code about board and strictly used keypress and draw function to loop over arraylist guessList of guesses and loop over the letters to print each letter and turn it a respective color, then I ncreated isWin function where all letter of iteration of guess list match the mysteryword. I made isLose function which is if guesslist is iterated over6 times and the is not isWin.

--- src/Board.java
+++ src/Board.java
@@ -8,126 +8,128 @@ import java.util.ArrayList;
         private SquareState squarestate;//make class
         private String word;
 
-        private ArrayList<String> guessList = new ArrayList<String>();
+        private char[][] guessList =new char[5][6];
 
         public Board(int x, int y, int width, int height, String[] previousGuesses, String currentGuess) {
             super();
             this.x = x;
             this.y = y;
-            this.size = size;
-            this.row = row;
-            this.column = column;
+//            this.size = size;
+//            this.row = row;
+//            this.column = column;
         }
 
         public Board() {
-            super();
-            this.x = 0;
-            this.y = 0;
-            this.size = 100;
-            this.row = 0;
-            this.column = 0;
-            this.squareState = BLANK;
-            this.word = getGuess();
-
+//            super();
+//            this.x = 0;
+//            this.y = 0;
+//            this.size = 100;
+//            this.row = 0;
+//            this.column = 0;
+//            this.squareState = BLANK;
+//            this.word = getGuess();
+//
 
         }
 
-        public void draw() {
-            for (int r = 0; r < 7; r++) {
-                for (int c = 0; c < 6; c++) {
-                    rect(x, y, row, column);
-                }
-            }
-        }
+//        public void draw() {
+//            for (int r = 0; r < 7; r++) {
+//                for (int c = 0; c < 6; c++) {
+//                    rect(x, y, row, column);
+//                }
+//            }
+//        }
+
+//
+//        public int getHeight() {
+////            return row;
+//        }
 
 
-        public int getHeight() {
-            return row;
-        }
 
-
-
-        public int getWidth() {
-            return column;
-        }
-
-        public int countOfGuesses() {//make arraylist to hold all guess list
-            return guessList.size();
-        }
+//        public int getWidth() {
+//            return column;
+//        }
+//
+//        public int countOfGuesses() {//make arraylist to hold all guess list
+//            return guessList.size();
+//        }
 
         //boolean method to indicate if game is still going or over
 
-        public boolean guessCorrect() {
-            if (guess.equals(word)) {
-                return true;
-            }
-        }
+//        public boolean guessCorrect() {
+////            if (guess.equals(word)) {
+////                return true;
+////            }
+//        }
 
 
 
-        public boolean isGameOver() {
-            if (guessCorrect() || guestList.size() == getHeight()) {
-                return true;
-                screenDisplay();
-            }
-        }
+//        public boolean isGameOver() {
+//            if (guessCorrect() || guestList.size() == getHeight()) {
+//                return true;
+//                screenDisplay();
+//            }
+//        }
 
-        public void screenDisplay(){
-        if(guessCorrect) {
-            System.out.println("You got it!");
-        }else{
-            System.out.println("Better Luck next time!");
-        }
-        }
-    }
+//        public void screenDisplay(){
+//        if(guessCorrect) {
+//            System.out.println("You got it!");
+//        }else{
+//            System.out.println("Better Luck next time!");
+//        }
+//        }
+
 
         //arraylist holding all guesses in respective game
-        public void guessList(String guess) {//arrarylist that holds all guesses in new game
-            while (!isGameOver) {
-                if (guess.size() == 5) {
-                    guessList.add(guess);
-                }
-            }
-        }
+//        public void guessList(String guess) {//arrarylist that holds all guesses in new game
+//            while (!isGameOver) {
+//                if (guess.size() == 5) {
+//                    guessList.add(guess);
+//                }
+////            }
+//        }
 
 
-//compares guess to word to evaluate which color each square will be
-        public void stateEvaluation() {
-            if (guess.equals(word)) {
-                for (int i = 0; i < getRow().length; i++) {
-                    squareState = SquareState.GREEN;
-                    isGameOver() == true;
-                }
-
-            } else {
-                for (int i = 0; i < guess.size(); i++) {
-                    if (guess.charAt(i) == word.charAt(i)) {
-                        squarestate = SquareState.GREEN;
-                    } else if (word.contains(guess.charAt(i))) {
-                        squarestate = SquareState.YELLOW;
-                    } else {
-                        squarestate = SquareState.BLANK;
-                    }
-                }
-            }
-        }
+////compares guess to word to evaluate which color each square will be
+//        public void stateEvaluation() {
+//            if (guess.equals(word)) {
+//                for (int i = 0; i < getRow().length; i++) {
+//                    squareState = SquareState.GREEN;
+//                    isGameOver() == true;
+//                }
+//
+//            } else {
+//                for (int i = 0; i < guess.size(); i++) {
+//                    if (guess.charAt(i) == word.charAt(i)) {
+//                        squarestate = SquareState.GREEN;
+//                    } else if (word.contains(guess.charAt(i))) {
+//                        squarestate = SquareState.YELLOW;
+//                    } else {
+//                        squarestate = SquareState.BLANK;
+//                    }
+//                }
+////            }
+//        }
 
 
         //draws color of each letter in row according to state evaluation
 
-        public void display(){
-            if(squarestate== SquareState.GREEN){
-                Main.app.fill(83, 207, 58);
-            }
-            else if (squarestate== SquareState.YELLOW){
-                Main.app.fill(247, 223, 84);
-            }
-            else{
-                Main.app.fill(255);
-            }
-            Main.app.rect(x,y,size,size);
+//        public void display(){
+//            if(squarestate== SquareState.GREEN){
+//                Main.app.fill(83, 207, 58);
+//            }
+//            else if (squarestate== SquareState.YELLOW){
+//                Main.app.fill(247, 223, 84);
+//            }
+//            else{
+//                Main.app.fill(255);
+//            }
+//            Main.app.rect(x,y,size,size);
 
+//        }
         }
 
-    }
-}
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,26 @@ import java.util.ArrayList;
 
 public class Main extends PApplet {
     private final String[] wordBank= {"towel", "phone", "sound", "watch"};
-    private String guess;
-    private ArrayList<String> guessList;
+    private String guess="";
+    private ArrayList<String> guessList = new ArrayList<String>();
 
+    private String mysteryWord="found";
+    public static Main app;
+
+    public static void main (String[] args){
+        PApplet.main("Main");
+    }
+
+    public Main(){
+        super();
+        app = this;
+    }
 
     public void setup() {
 
-
-
-        board = Board(200, 100, 200, 500);
+//
+//
+//        board = Board(200, 100, 200, 500);
 
 
     }
@@ -27,23 +38,64 @@ public class Main extends PApplet {
 
         public void draw () {
             //start button
-            rect(100, 200, 100, 50);
+           // rect(100, 200, 100, 50);
 
             //enter button
 
-            rect(300, 200, 90, 50);
+         //   rect(300, 200, 90, 50);
 
             //example of object to create
 
             //draw print letters
            // for(int i=i;i<)
-            board.draw();
+//            board.draw();
+
+            // print the guess list
+            for(int i=0; i<guessList.size();i++) {
+                String word = guessList.get(i);
+                for (int j = 0; j < word.length(); j++) {
+                    char letter = word.charAt(j);
+
+                    if(letter==mysteryWord.charAt(j)) {
+                        // green: letter is correct and in correct place
+                        fill(0, 255, 0);
+                    } else if(mysteryWord.indexOf(letter)>=0){
+                        fill(255,255,0);
+
+                    }
+
+                    else {
+                        // gray: letter is not in the word at all
+                        fill(120);
+                    }
+
+                    // yellow: letter is in the word, but in the incorrect place
+                    text(letter, j * 50 + 50, i * 60 + 60);
+                }
+            }
+
+            // print the guess
+            for(int i=0;i<guess.length();i++){
+                fill(255);
+                text(guess.charAt(i),50+(50*i),guessList.size() * 60 + 60);
+            }
+
+           if(isWin()) {
+               fill(255);
+               text("You got it!", 180, 400);
+           }
+
+           if(youLose()){
+               fill(255);
+               text("Loser",180,400);
+           }
 
         }
 //generate index of array to choose random word
-        public int getIndexWord () {
+        public int getIndexWord() {
 
             int x = (int) (Math.random() * wordBank.length);
+            return x;
         }
 
         // mouseclick for start button if mouse clicked reset play game
@@ -53,11 +105,19 @@ public class Main extends PApplet {
                 playGame();
             }
 
-            // enter button
-            if (mouseX > 300 && mouseX < 390 && mouseY > 150 && mouseY < 200) ;//fix
-            check();//write method
+//            // enter button
+//            if (mouseX > 300 && mouseX < 390 && mouseY > 150 && mouseY < 200) ;//fix
+//            check();//write method
         }
 
+        public boolean isWin(){
+           return (guessList.size()>0 && guessList.get(guessList.size()-1).equals(mysteryWord));
+        }
+
+        public boolean youLose() {
+            return (guessList.size() == 6 && !isWin());
+
+        }
     public void playGame() {
         String word = wordBank[getIndexWord()];
         System.out.println("Take a guess and click enter to submit!");
@@ -72,16 +132,24 @@ public class Main extends PApplet {
     //key pressed to guess
 
     public void keyPressed() {
-        // guess="";
-        if (!isGameOver() && guess.length() < 5) {
+        if ( guess.length() < 5&& guessList.size()<6) {
             guess = guess + key;
+       }
+        if(guess.length()==5){
+            guessList.add(guess);
+            guess = "";
+            System.out.println(guessList);
+
+
+
         }
-        if (guess.length() == 5) {
-            stateEvaluation();// call state
+      // if (guess.length() == 5) {
+        //    stateEvaluation();// call state
             // System.out.println("Click Enter!")
 
 
-            guessList(guess);//add to check
-        }
+      //      guessList(guess);//add to check
+      //  }
+    System.out.println(guess);
     }
 }
