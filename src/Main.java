d7fceeb539d7fd4073b4f0307fabbde658a77baa
julia-karsner <25jkarsner@castilleja.org>
2023-05-26 12:44:19 -0700

    I created grid around letters, and added a method call to keypressed to ensure that key is letter. I also played arounf with a delete key triggering a letter to be removed, but didn't keep it.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+
+
 import processing.core.PApplet;
 
 import java.util.ArrayList;
@@ -6,7 +8,7 @@ import java.util.ArrayList;
 
 
 public class Main extends PApplet {
-    private final String[] wordBank= {"towel", "phone", "sound", "watch","petal","metal","cards","stilt","route","shaky","vegan","power","dowel","trust"};
+    private final String[] wordBank= {"towel", "phone", "sound", "watch","petal","metal","cards","stilt","route","shaky","vegan","power","dowel","trust","candy"};
     private String guess="";
     private ArrayList<String> guessList = new ArrayList<String>();
 
@@ -23,8 +25,18 @@ public class Main extends PApplet {
     }
 
     public void setup() {
+       // g.line(50,40,90,100);
         textSize(27);
          mysteryWord=wordBank[getIndexWord()];
+      // size(400, 400);
+       // line(120, 80, 340, 300);
+        //fill(255);
+
+        //stroke(126);
+        //line(340, 80, 60, 340, 300, 0);
+        //stroke(255);
+        //line(340, 300, 0, 120, 300, -200);
+
 
 //
 //
@@ -40,74 +52,80 @@ public class Main extends PApplet {
 
         public void draw () {
 
-            background(0);
+           background(0);
+
+            fill(194, 239, 242);
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    rect(j * 50 + 70, i * 60 + 25, 50, 50);
+                }
+            }
 
 
-            //start button
-            fill(200,100,5);
-            rect(120, 500, 170, 80);
-            fill(0);
-            text("RESET",  170,550);
+                //start button
+                fill(200, 100, 5);
+                rect(120, 500, 170, 80);
+                fill(0);
+                text("RESET", 170, 550);
 
 
+                //enter button
 
-            //enter button
+                //   rect(300, 200, 90, 50);
 
-         //   rect(300, 200, 90, 50);
+                //example of object to create
 
-            //example of object to create
-
-            //draw print letters
-           // for(int i=i;i<)
+                //draw print letters
+                // for(int i=i;i<)
 //            board.draw();
 
-            // print the guess list
-            for(int i=0; i<guessList.size();i++) {
-                String word = guessList.get(i);
-                for (int j = 0; j < word.length(); j++) {
-                    char letter = word.charAt(j);
+                // print the guess list
+                for (int i = 0; i < guessList.size(); i++) {
+                    String word = guessList.get(i);
+                    for (int j = 0; j < word.length(); j++) {
+                        char letter = word.charAt(j);
 
-                    if(letter==mysteryWord.charAt(j)) {
-                        // green: letter is correct and in correct place
-                        fill(0, 255, 0);
-                    } else if(mysteryWord.indexOf(letter)>=0){
-                        fill(255,255,0);
+                        if (letter == mysteryWord.charAt(j)) {
+                            // green: letter is correct and in correct place
+                            fill(0, 255, 0);
+                        } else if (mysteryWord.indexOf(letter) >= 0) {
+                            fill(255, 255, 0);
+
+                        } else {
+                            // gray: letter is not in the word at all
+                            fill(120);
+                        }
+
+                        // yellow: letter is in the word, but in the incorrect place
+
+                        text(letter, j * 50 + 90, i * 60 + 60);
 
                     }
 
-                    else {
-                        // gray: letter is not in the word at all
-                        fill(120);
-                    }
-
-                    // yellow: letter is in the word, but in the incorrect place
-                    text(letter, j * 50 + 50, i * 60 + 60);
                 }
 
+                // print the guess
+                for (int i = 0; i < guess.length(); i++) {
+                    fill(255);
+                    text(guess.charAt(i), 90 + (50 * i), guessList.size() * 60 + 60);
+                }
+
+                if (isWin()) {
+                    fill(255);
+                    text("You got it!", 130, 450);
+                }
+
+                if (youLose()) {
+                    fill(255);
+                    text("Loser", 160, 450);
+                    text("The correct word is: " + mysteryWord, 90, 490);
+                }
+                if (guessList.size() > 0 && !isWin() && !youLose()) {//make so appear
+                    fill(255);
+                    text("Guess Again", 130, 450);
+                }
             }
 
-            // print the guess
-            for(int i=0;i<guess.length();i++){
-                fill(255);
-                text(guess.charAt(i),50+(50*i),guessList.size() * 60 + 60);
-            }
-
-           if(isWin()) {
-               fill(255);
-               text("You got it!", 130, 450);
-           }
-
-           if(youLose()){
-               fill(255);
-               text("Loser",160,450);
-               text("The correct word is: "+ mysteryWord,90,490);
-           }
-        if(guessList.size()>0 && !isWin()&&!youLose()) {//make so appear
-               fill(255);
-              text("Guess Again", 130, 450);
-           }
-
-        }
 //generate index of array to choose random word
         public int getIndexWord() {
 
@@ -141,7 +159,10 @@ public class Main extends PApplet {
         guessList=new ArrayList<String>();
         guess="";
        // String word = wordBank[getIndexWord()];
+
+        mysteryWord=wordBank[getIndexWord()];
         System.out.println("Take a guess and click enter to submit!");
+
 
         //call to enter method
 
@@ -153,13 +174,24 @@ public class Main extends PApplet {
     //key pressed to guess
 
     public void keyPressed() {
-        if ( guess.length() < 5&& guessList.size()<6&& !isWin()) {
+
+        if(key==DELETE && guess.length()>0){
+            guess=guess.substring(0,guess.length()-1);
+        }else if ( Character.isAlphabetic(key) && guess.length() < 5&& guessList.size()<6&& !isWin() ) {
             guess = guess + key;
        }
-        if(guess.length()==5){
+
+
+        if(guess.length()==5) {
             guessList.add(guess);
             guess = "";
             System.out.println(guessList);
+
+        }//if(guessList.size()>0&& key=='\n'){
+//        public void line(float x1, float y1, float z1,
+//        float x2, float y2, float z2) {
+//            g.line(x1, y1, z1, x2, y2, z2);
+//        }
 
 
 
@@ -171,6 +203,6 @@ public class Main extends PApplet {
 
       //      guessList(guess);//add to check
       //  }
-    System.out.println(guess);
+    //System.out.println(guess);
     }
-}
+
